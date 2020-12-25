32797eafea6e38cfa3faa2cd29ee347bc8877057
Sergei Iurevich <lask2su@gmail.com>
2020-12-25 21:17:39 +0300

    Run prettier lint:fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     "import/prefer-default-export": "off",
