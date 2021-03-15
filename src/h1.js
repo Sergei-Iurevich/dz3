f8a0ca15220344723a522078157e95ac23a68861
Sergei Iurevich <lask2su@gmail.com>
2021-03-15 19:39:37 +0300

    Change name functions

--- src/h1.js
+++ src/h1.js
@@ -1,22 +1,20 @@
-function run1() {
+export function multiplicationAndSum() {
   const a = 3;
   const b = 7;
   console.log(a + b);
   console.log(a * b);
 }
 
-function run2() {
+export function sumLenStrings() {
   const str1 = "asas";
   const str2 = "et";
   console.log(str1.length + str2.length);
 }
 
-function run3() {
+export function sumDigital() {
   const x = prompt("enter a three-digit number");
   const a = Number(x[0]);
   const b = Number(x[1]);
   const c = Number(x[2]);
   console.log(a + b + c);
 }
-
-module.exports = { run1, run2, run3 };
