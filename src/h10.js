5fbabc4a7e4e7edc70adacc38ac37c38f18a5c0a
Sergei Iurevich <lask2su@gmail.com>
2021-03-16 21:40:54 +0300

    Change src/h10.js, src/h10.test.js

--- src/h10.js
+++ src/h10.js
@@ -1,7 +1,8 @@
-export function task1() {
+export function checkDate() {
   const str = prompt("Enter a string");
-  // eslint-disable-next-line max-len
-  const regexpDate = /^(?:0|1|2|3(?![2-9]))\d(?<!00)(?:\.(?=.*\.)|-(?=.*\-))[0-1][0-2](?<!00)[\.-]\d{4}$/;
+  const strA = `^(?:0|1|2|3(?![2-9]))\\d(?<!00)(?:\\.(?=.*\\.)|`;
+  const strB = `-(?=.*\\-))[0-1][0-2](?<!00)[\\.-]\\d{4}$`;
+  const regexpDate = new RegExp(strA + strB);
   const regexpEmail = /^[-.\w]+@([\w-]+\.)+[\w-]+$/i;
   const regexpPhone = /^\+?\d\(\d{3}\)(?:-?\d){1,7}$/;
   const resDate = regexpDate.test(str);
--- src/h10.test.js
+++ src/h10.test.js
@@ -1,42 +1,42 @@
-import { task1 } from "./h10";
+import { checkDate } from "./h10";
 
 describe("Homework_10", () => {
-  describe("task1", () => {
+  describe("checkDate", () => {
     it("dd.mm.yyyy", () => {
       jest.spyOn(window, "prompt").mockReturnValue("12.01.2020");
-      expect(task1()).toBe("Date");
+      expect(checkDate()).toBe("Date");
     });
     it("dd-mm-yyyy", () => {
       jest.spyOn(window, "prompt").mockReturnValue("12-01-2020");
-      expect(task1()).toBe("Date");
+      expect(checkDate()).toBe("Date");
     });
     it("wrong date", () => {
       jest.spyOn(window, "prompt").mockReturnValue("12.01-2020");
-      expect(task1()).toBe("Invalid string");
+      expect(checkDate()).toBe("Invalid string");
     });
     it("mail", () => {
       jest.spyOn(window, "prompt").mockReturnValue("lisa.lol-is@mail.com");
-      expect(task1()).toBe("Email");
+      expect(checkDate()).toBe("Email");
     });
     it("wrong mail", () => {
       jest.spyOn(window, "prompt").mockReturnValue("lisa.lol-is@mail.(com");
-      expect(task1()).toBe("Invalid string");
+      expect(checkDate()).toBe("Invalid string");
     });
     it("Number Phone1", () => {
       jest.spyOn(window, "prompt").mockReturnValue("8(915)1235103");
-      expect(task1()).toBe("Number phone");
+      expect(checkDate()).toBe("Number phone");
     });
     it("Number Phone2", () => {
       jest.spyOn(window, "prompt").mockReturnValue("+7(915)123-51-03");
-      expect(task1()).toBe("Number phone");
+      expect(checkDate()).toBe("Number phone");
     });
     it("Wrong Number Phone1", () => {
       jest.spyOn(window, "prompt").mockReturnValue("-7(915)1235103");
-      expect(task1()).toBe("Invalid string");
+      expect(checkDate()).toBe("Invalid string");
     });
     it("Wrong Number Phone2", () => {
       jest.spyOn(window, "prompt").mockReturnValue("8(915)1235--1030");
-      expect(task1()).toBe("Invalid string");
+      expect(checkDate()).toBe("Invalid string");
     });
   });
 });
